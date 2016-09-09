bc81cf74ce7283b006ad1a0967271a86a5f990e7
Nguyen Nhu Tuan <tuanquynh0508@gmail.com>
2016-09-09 18:15:55 +0700

    Init Project

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# sym3
-Symfony 3 Lab
+sym3
+====
+
+A Symfony project created on September 8, 2016, 2:31 pm.
+
+```
+<VirtualHost *:80>
+    ServerName sym3.local
+    ServerAlias sym3.local
+	SetEnv sfEnv dev
+
+	#For Linux
+    DocumentRoot /home/nntuan/Gits/sym3/web
+	#For Windows
+    #DocumentRoot D:/projects/sym3/web
+
+	#For Linux
+    <Directory /home/nntuan/Gits/sym3/web>
+	#For windows
+    #<Directory D:/projects/sym3/web>
+        #Options Indexes FollowSymLinks
+        AllowOverride all
+        Require all granted
+        <IfModule mod_rewrite.c>
+            RewriteEngine On
+            RewriteCond %{REQUEST_FILENAME} !-f
+            RewriteRule ^(.*)$ app.php [QSA,L]
+            RewriteCond %{HTTP:Authorization} ^(.*)
+            RewriteRule .* - [e=HTTP_AUTHORIZATION:%1]
+        </IfModule>
+    </Directory>
+    #For Ubuntu apache config
+    ErrorLog ${APACHE_LOG_DIR}/error-sym3.log
+    CustomLog ${APACHE_LOG_DIR}/access-sym3.log combined
+
+    #For Xampp windows config
+    #ErrorLog "logs/error-sym3.log"
+    #CustomLog "logs/access-sym3.log" combined
+</VirtualHost>
+```
+
+#Commands
+```
+tail -f -n 100 /var/log/apache2/access-sym3.log
+tail -f -n 100 /var/log/apache2/error-sym3.log
+
+php bin/console
+php bin/console cache:clear --env=dev
+
+php bin/console assets:install --symlink web
+php bin/console assetic:dump --env=dev
+
+php bin/console doctrine:migrations:diff --env=dev
+php bin/console doctrine:migrations:migrate 20160321225157 --env=dev
+
+php bin/console generate:bundle --namespace=TuanQuynh/UserBundle --dir=src --format=annotation --no-interaction
+
+php bin/console doctrine:generate:entities AppBundle/Entity/User
+php bin/console doctrine:schema:update --force
+
+php bin/console app:add-user
+```
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ * @ORM\Table(name="symfony_demo_user")
+ *
+ * Defines the properties of the User entity to represent the application users.
+ * See http://symfony.com/doc/current/book/doctrine.html#creating-an-entity-class
+ *
+ * Tip: if you have an existing database, you can generate these entity class automatically.
+ * See http://symfony.com/doc/current/cookbook/doctrine/reverse_engineering.html
+ *
+ * @author Ryan Weaver <weaverryan@gmail.com>
+ * @author Javier Eguiluz <javier.eguiluz@gmail.com>
+ */
+class User implements AdvancedUserInterface, \Serializable
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @ORM\Column(type="json_array")
+     */
+    private $roles = [];
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * Returns the roles or permissions granted to the user for security.
+     */
+    public function getRoles()
+    {
+        $roles = $this->roles;
+
+        // guarantees that a user always has at least one role for security
+        if (empty($roles)) {
+            $roles[] = 'ROLE_ADMIN';
+        }
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles)
+    {
+        $this->roles = $roles;
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     */
+    public function getSalt()
+    {
+        // See "Do you need to use a Salt?" at http://symfony.com/doc/current/cookbook/security/entity_provider.html
+        // we're using bcrypt in security.yml to encode the password, so
+        // the salt value is built-in and you don't have to generate one
+
+        return;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     */
+    public function eraseCredentials()
+    {
+        // if you had a plainPassword property, you'd nullify it here
+        // $this->plainPassword = null;
+    }
+
+    public function isAccountNonExpired()
+    {
+        return true;
+    }
+
+    public function isAccountNonLocked()
+    {
+        return true;
+    }
+
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    public function isEnabled()
+    {
+        return $this->isActive;
+    }
+
+    // serialize and unserialize must be updated - see below
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+            $this->isActive
+        ));
+    }
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+            $this->isActive
+        ) = unserialize($serialized);
+    }
+}
